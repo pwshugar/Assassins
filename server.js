8dde4712625d0282bfc4939783ca1582091a2c0c
Peter Shugar <pwshugar@gmail.com>
2013-07-24 18:08:43 -0700

    installed socket.io, server displaying test html

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var index = require('./index.html');
+// var app = express();
 
-app.get('/', function(req, res){
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
-});
+// // app.get('/', function(req, res){
+// //   var body = index.html;
+// //   res.setHeader('Content-Type', 'text/plain');
+// //   res.setHeader('Content-Length', body.length);
+// //   res.end(body);
+// // });
 
-app.get('/', function(req, res){
-  res.send('Hello World');
-  console.log('tried');
-});
+// app.get('/', function(req, res){
+//   console.log("GET")
+//   res.send(index.html);
+// });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+// app.listen(3000);
+// console.log('Listening on port 3000');
 
 // var mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/test');
@@ -24,3 +25,19 @@ console.log('Listening on port 3000');
 // console.log('Listening on ' + "port" + '...');
 // db.once('open', function callback () {
 // });npm install
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
