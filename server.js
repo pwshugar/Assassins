f49c4590a6fd1c340e08bbf08c30b38e12ac0d23
Peter Shugar <pwshugar@gmail.com>
2013-07-25 17:46:22 -0700

    server can redirect and accept posts

--- server.js
+++ server.js
@@ -25,19 +25,95 @@
 // console.log('Listening on ' + "port" + '...');
 // db.once('open', function callback () {
 // });npm install
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
 
-server.listen(8080);
 
-app.get('/', function (req, res) {
+// var app = require('express')()
+//   , server = require('http').createServer(app)
+//   , io = require('socket.io').listen(server);
+
+// server.listen(8080);
+
+// app.get('/', function (req, res) {
+//   res.sendfile(__dirname + '/index.html');
+// });
+
+// io.sockets.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
+
+var express = require('express');
+var app = express();
+
+app.use(express.cookieParser());
+app.use(express.session({secret: '1234567890QWERTY'}));
+// app.use(express.session({
+//   store: new MongoStore({
+//     url: 'mongodb://root:myPassword@mongo.onmodulus.net:27017/3xam9l3'
+//   }),
+//   secret: '1234567890QWERTY'
+// }));
+app.post('/login', function(req, res){
+	console.log('sent');
+	res.end('Worked.');
+});
+
+app.get('/', function(req, res){
+  if(!req.session.user) {
+    res.redirect('/login');
+  } else {
+    res.redirect('/home');
+  }
+});
+
+app.get('/home', function(req, res){
+  if(!req.session.user) {
+    res.redirect('/login');
+  } else {
+    res.end('Your Home.');
+  }
+});
+
+app.get('/login', function(req, res) {
+  // if(req.session.lastPage) {
+  //   res.write('Last page was: ' + req.session.lastPage + '. ');
+  // }
+
+  // req.session.lastPage = '/login';
   res.sendfile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+app.post('/')
+
+// app.get('/awesome', function(req, res) {
+//   if(req.session.lastPage) {
+//     res.write('Last page was: ' + req.session.lastPage + '. ');
+//   }
+//   req.session.lastPage = '/awesome';
+//   res.end('Your Awesome.');
+// });
+
+// app.get('/radical', function(req, res) {
+//   if(req.session.lastPage) {
+//     res.write('Last page was: ' + req.session.lastPage + '. ');
+//   }
+
+//   req.session.lastPage = '/radical';
+//   res.end('What a radical visit!');
+// });
+
+// app.get('/tubular', function(req, res) {
+//   if(req.session.lastPage) {
+//     res.write('Last page was: ' + req.session.lastPage + '. ');
+//   }
+
+//   req.session.lastPage = '/tubular';
+//   res.end('Are you a surfer?');
+// });
+
+app.listen(process.env.PORT || 8080);
+
+
+
